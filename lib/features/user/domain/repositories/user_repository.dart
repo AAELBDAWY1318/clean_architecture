17bf18f3ca42025aacdc582b37e6c96b7ebd9a6f
AAELBDAWY1318 <abdullahawad1318@gmail.com>
2024-09-21 04:15:28 +0300

    create user repository in domation layer

---
+++ lib/features/user/domain/repositories/user_repository.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:male_app/core/errors/failure.dart';
+import 'package:male_app/features/user/domain/entities/user_entity.dart';
+
+abstract class UserRepository {
+  Future<Either<Failure, UserEntity>> getUser();
+}
