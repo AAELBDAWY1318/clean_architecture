662b803d88dc67b137ee152fc398441ce9b7ed14
AAELBDAWY1318 <abdullahawad1318@gmail.com>
2024-09-21 04:24:07 +0300

    add user parameters

--- lib/features/user/domain/repositories/user_repository.dart
+++ lib/features/user/domain/repositories/user_repository.dart
@@ -1,7 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:male_app/core/errors/failure.dart';
+import 'package:male_app/core/param/parameter.dart';
 import 'package:male_app/features/user/domain/entities/user_entity.dart';
 
 abstract class UserRepository {
-  Future<Either<Failure, UserEntity>> getUser();
+  Future<Either<Failure, UserEntity>> getUser({required UserParams params});
 }
--- lib/features/user/domain/use_case/get_user.dart
+++ lib/features/user/domain/use_case/get_user.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:male_app/core/errors/failure.dart';
+import 'package:male_app/core/param/parameter.dart';
 import 'package:male_app/features/user/domain/entities/user_entity.dart';
 import 'package:male_app/features/user/domain/repositories/user_repository.dart';
 
@@ -7,7 +8,7 @@ class GetUser {
   final UserRepository repository;
 
   GetUser({required this.repository});
-  Future<Either<Failure, UserEntity>> call() {
-    return repository.getUser();
+  Future<Either<Failure, UserEntity>> call({required UserParams params}) {
+    return repository.getUser(params: params);
   }
 }
