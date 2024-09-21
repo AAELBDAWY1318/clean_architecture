c5e590b2523c1ce68402ecfe85b50c3ddb0af793
AAELBDAWY1318 <abdullahawad1318@gmail.com>
2024-09-21 04:19:52 +0300

    add get user usecase in domain layer

---
+++ lib/features/user/domain/use_case/get_user.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:male_app/core/errors/failure.dart';
+import 'package:male_app/features/user/domain/entities/user_entity.dart';
+import 'package:male_app/features/user/domain/repositories/user_repository.dart';
+
+class GetUser {
+  final UserRepository repository;
+
+  GetUser({required this.repository});
+  Future<Either<Failure, UserEntity>> call() {
+    return repository.getUser();
+  }
+}
